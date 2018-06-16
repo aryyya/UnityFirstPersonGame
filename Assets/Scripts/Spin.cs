ec506c4bb50b13373c441a74c979747582827ad4
aryyya <arya@live.ca>
2018-06-15 20:00:19 -0700

    Add automatic spin to player.

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    public float speed = 3.0f;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        transform.Rotate(0.0f, speed, 0.0f);
+    }
+}
