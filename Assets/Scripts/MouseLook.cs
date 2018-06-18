85440c04c7a307b6a7ed0fef7fcb48b7d223f977
aryyya <arya@live.ca>
2018-06-17 21:21:36 -0700

    Complete mouse look functionality.

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -7,6 +7,12 @@ public class MouseLook : MonoBehaviour
     public float horizontalSensitivity = 3.0f;
     public float verticalSensitivity = 3.0f;
 
+    public float minimumVerticalAngle = -45.0f;
+    public float maximumVerticalAngle = +45.0f;
+
+    private float _rotationX = 0.0f;
+    private float _rotationY = 0.0f;
+
     public enum RotationAxis
     {
         MouseXAndY = 0,
@@ -22,15 +28,17 @@ public class MouseLook : MonoBehaviour
 
     void Update()
     {
+        /*
         if (axes == RotationAxis.MouseX)
         {
-            float horizontalTransform = Input.GetAxis("Mouse X") * horizontalSensitivity;
-            transform.Rotate(0.0f, horizontalTransform, 0.0f);
+            _rotationY += Input.GetAxis("Mouse X") * horizontalSensitivity;
+            transform.localEulerAngles = new Vector3(_rotationX, _rotationY);
         }
         else if (axes == RotationAxis.MouseY)
         {
-            float verticalTransform = Input.GetAxis("Mouse Y") * verticalSensitivity;
-            transform.Rotate(verticalTransform, 0.0f, 0.0f);
+            _rotationX -= Input.GetAxis("Mouse Y") * verticalSensitivity;
+            _rotationX = Mathf.Clamp(_rotationX, minimumVerticalAngle, maximumVerticalAngle);
+            transform.localEulerAngles = new Vector3(_rotationX, _rotationY, 0);
         }
         else
         {
@@ -38,5 +46,11 @@ public class MouseLook : MonoBehaviour
             float verticalTransform = Input.GetAxis("Mouse Y") * verticalSensitivity;
             transform.Rotate(-verticalTransform, horizontalTransform, 0.0f);
         }
+        */
+
+        _rotationX -= Input.GetAxis("Mouse Y") * verticalSensitivity;
+        _rotationY += Input.GetAxis("Mouse X") * horizontalSensitivity;
+        _rotationX = Mathf.Clamp(_rotationX, minimumVerticalAngle, maximumVerticalAngle);
+        transform.localEulerAngles = new Vector3(_rotationX, _rotationY);
     }
 }
