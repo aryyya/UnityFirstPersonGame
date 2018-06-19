41a674419035fbcf0ffbf013e1a07817b11ef74e
aryyya <arya@live.ca>
2018-06-19 16:35:34 -0700

    Add basic raycast gun component.

---
+++ Assets/Scripts/RayShooter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayShooter : MonoBehaviour
+{
+    private Camera _camera;
+
+    void Start()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 point = new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2, 0);
+            Ray ray = _camera.ScreenPointToRay(point);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("Hit " + hit.point);
+            }
+        }
+    }
+}
