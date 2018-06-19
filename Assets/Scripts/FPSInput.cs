8d2b77c4313457d81f02c8085799f2f934638def
aryyya <arya@live.ca>
2018-06-18 17:12:07 -0700

    Add keyboard control.

---
+++ Assets/Scripts/FPSInput.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSInput : MonoBehaviour
+{
+    public float speed = 6.0f;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Sprint();
+
+        float deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float deltaY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        transform.Translate(deltaX, 0.0f, deltaY);
+    }
+
+    private void Sprint()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed *= 2.0f;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed /= 2.0f;
+        }
+    }
+}
