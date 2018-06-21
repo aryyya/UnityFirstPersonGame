cdb046f1daab5660e556e0b6a6b0765cccb895f2
aryyya <arya@live.ca>
2018-06-21 01:15:06 -0700

    Add damage to player on fireball contact.

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {
+    [SerializeField] private int _health = 5;
+
     void Start()
     {
     }
 
     void Update()
     {
+    }
+
+    public void Hurt(int damage)
+    {
+        _health -= damage;
+        Debug.Log("Player health: " + _health);
+
+        if (_health < 1)
+        {
+            Debug.Log("Player is dead.");
+        }
     }
 }
