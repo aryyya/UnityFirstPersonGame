ac8a8c7fffe5d8ba527d7fac86ee62a86f10fe4a
aryyya <arya@live.ca>
2018-06-22 22:45:31 -0700

    Make sprint speed an inspector editable value.

--- Assets/Scripts/FPSInput.cs
+++ Assets/Scripts/FPSInput.cs
@@ -7,24 +7,27 @@ using UnityEngine;
 public class FPSInput : MonoBehaviour
 {
     public float speed = 6.0f;
+    public float sprintSpeed = 8.0f;
     public float gravity = -9.8f;
 
+    private float currentSpeed;
     private CharacterController _characterController;
 
     void Start()
     {
         _characterController = GetComponent<CharacterController>();
+        currentSpeed = speed;
     }
 
     void Update()
     {
         Sprint();
 
-        float deltaX = Input.GetAxis("Horizontal") * speed;
-        float deltaZ = Input.GetAxis("Vertical") * speed;
+        float deltaX = Input.GetAxis("Horizontal") * currentSpeed;
+        float deltaZ = Input.GetAxis("Vertical") * currentSpeed;
 
         Vector3 movement = new Vector3(deltaX, 0.0f, deltaZ);
-        movement = Vector3.ClampMagnitude(movement, speed);
+        movement = Vector3.ClampMagnitude(movement, currentSpeed);
         movement.y = gravity;
         movement *= Time.deltaTime;
         movement = transform.TransformDirection(movement);
@@ -36,11 +39,11 @@ public class FPSInput : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            speed *= 2.0f;
+            currentSpeed = sprintSpeed;
         }
         else if (Input.GetKeyUp(KeyCode.LeftShift))
         {
-            speed /= 2.0f;
+            currentSpeed = speed;
         }
     }
 }
